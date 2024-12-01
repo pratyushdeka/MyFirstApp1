ed5eb67d190363e718463e91fd049ba6ebaf7896
Pratyush Deka <pratyushdeka255@gmail.com>
2024-12-02 02:09:45 +0530

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Pratyush Kumar Deka on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyFirstAppTests/MyFirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppTests.swift
+//  MyFirstAppTests
+//
+//  Created by Pratyush Kumar Deka on 02/12/24.
+//
+
+import Testing
+@testable import MyFirstApp
+
+struct MyFirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
